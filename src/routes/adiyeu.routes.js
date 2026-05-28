13e6f420bc39dc8980ad79803b081c261c0b0fed
mouhaseck208 <mouhaseck208@gmail.com>
2026-05-28 18:58:13 +0000

    Fix adiyeu routes

--- src/routes/adiyeu.routes.js
+++ src/routes/adiyeu.routes.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth.middleware');
-const { creerAdiyeu, listerAdiyeu, marquerPaye, totalCollecte } = require('../controllers/adiyeu.controller');
+const { creerAdiyeu, listerAdiyeu, marquerPaye } = require('../controllers/adiyeu.controller');
 
 router.post('/', auth, creerAdiyeu);
 router.get('/', auth, listerAdiyeu);
-router.put('/:id/payer', auth, marquerPaye);
-router.get('/total', auth, totalCollecte);
+router.put('/:id', auth, marquerPaye);
 
 module.exports = router;
