53c20fc37ed87dda2f0e17e56ecb25a41187c310
mouhaseck208 <mouhaseck208@gmail.com>
2026-05-28 19:29:46 +0000

    Fix auth routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const { registerDaara, login } = require('../controllers/auth.controller');
+const Daara = require('../models/Daara');
+const { registerDaara, login, loginMembre } = require('../controllers/auth.controller');
 
 router.post('/register', registerDaara);
 router.post('/login', login);
-const Daara = require('../models/Daara');
+router.post('/login-membre', loginMembre);
 
 router.get('/search', async (req, res) => {
   try {
     const { nom } = req.query;
-    const daaras = await Daara.find({ 
-      nom: new RegExp(nom, 'i') 
+    const daaras = await Daara.find({
+      nom: new RegExp(nom, 'i')
     }).select('nom siege');
     res.json(daaras);
   } catch (err) {
     res.status(500).json({ message: 'Erreur serveur' });
   }
 });
+
 module.exports = router;
-router.post('/login-membre', loginMembre);
