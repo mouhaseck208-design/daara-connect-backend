ad1a632468eaea8ea7a5815d6bc6296124b68b86
mouhaseck208 <mouhaseck208@gmail.com>
2026-05-29 00:45:20 +0000

    Fix upload limit 50mb

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ app.use(cors({
 
 app.use(express.json());
 
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 // Routes
 const authRoutes         = require('./src/routes/auth.routes');
 const kourelRoutes       = require('./src/routes/kourel.routes');
